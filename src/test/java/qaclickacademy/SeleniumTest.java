adcd11725a2cf0b1e1c392c5d2287c7558a95bf6
rober <robertreyesjr@gmail.com>
2022-03-06 16:38:47 -0600

    first commit

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+import org.testng.annotations.Test;
+//import org.junit.Test;
+
+public class SeleniumTest {
+
+    @Test
+    public void BrowserAutomation(){
+        System.out.println("BrowserAutomation");
+    }
+
+    @Test
+    public void elementsUi(){
+        System.out.println("elementsUi");
+    }
+
+}
